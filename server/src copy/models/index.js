3835b82752bc3b3a264b71bcf4910c55bd8ecdec
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-11-09 13:37:01 -0300

    add .

--- server/src copy/models/index.js
+++
@@ -1,9 +0,0 @@
-import pkg from 'sequelize';
-const { Sequelize, Model, DataTypes } = pkg;
-
-const sequelize = new Sequelize('crud_sequelize', 'root', 'example', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
-
-export { sequelize };
--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,5 +1,5 @@
 import pkg from 'sequelize';
-const { Sequelize, Model, DataTypes } = pkg;
+const { Sequelize } = pkg;
 
 const sequelize = new Sequelize('crud_sequelize', 'postgres', 'example', {
     host: 'localhost',
